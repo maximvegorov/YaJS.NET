7832f6e0a8251265d5cf5d373269fe2709eef450
Maxim Egorov <maxim.v.egorov@gmail.com>
2015-01-27 10:53:49 +0300

    re #59

---
+++ Runtime.Tests/JSValueTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using YaJS.Runtime.Exceptions;
+
+namespace YaJS.Runtime.Tests {
+	[TestClass]
+	public sealed class JSValueTests {
+		[TestMethod]
+		[ExpectedException(typeof(TypeErrorException))]
+		public void InstanceOf_Undefined() {
+			var compiler = new Mock<ICompilerServices>();
+			var vm = new VirtualMachine(compiler.Object);
+
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(TypeErrorException))]
+		public void InstanceOf_Simple() {
+			var compiler = new Mock<ICompilerServices>();
+			var vm = new VirtualMachine(compiler.Object);
+
+		}
+	}
+}
