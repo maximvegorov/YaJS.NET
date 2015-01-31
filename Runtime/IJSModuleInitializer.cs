4770ce16efe21f0add09a9d5558e012348af7e9f
Maxim Egorov <maxim.v.egorov@gmail.com>
2015-02-01 00:40:08 +0300

    fixes #43

---
+++ Runtime/IJSModuleInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace YaJS.Runtime {
+	/// <summary>
+	/// Интерфейс будет использоваться для расширения глобального объекта
+	/// виртуальной машины расширенными объектами и функциями
+	/// </summary>
+	[ContractClass(typeof(ContractClassForIJSModuleInitializer))]
+	public interface IJSModuleInitializer {
+		void ApplyTo(VirtualMachine vm);
+	}
+
+	[ContractClassFor(typeof(IJSModuleInitializer))]
+	internal abstract class ContractClassForIJSModuleInitializer : IJSModuleInitializer {
+		public void ApplyTo(VirtualMachine vm) {
+			Contract.Requires<ArgumentNullException>(vm != null);
+		}
+	}
+}
